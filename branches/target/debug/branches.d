227bf596277c899e7aa7cd797fa629cf7449e2aa
See <mac@MacdeMacBook-Pro.local>
2023-01-14 13:47:11 +0800

    remove trivial files

--- branches/target/debug/branches.d
+++
@@ -1,1 +0,0 @@
-/Users/mac/Documents/RUST_learn_git/branches/target/debug/branches: /Users/mac/Documents/RUST_learn_git/branches/src/main.rs
--- branches/target/debug/deps/branches-34ea44575405ccfd.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/Documents/RUST_learn_git/branches/target/debug/deps/branches-34ea44575405ccfd: src/main.rs
-
-/Users/mac/Documents/RUST_learn_git/branches/target/debug/deps/branches-34ea44575405ccfd.d: src/main.rs
-
-src/main.rs:
